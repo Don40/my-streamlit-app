e34560a0c7be9371001bd5775d6a74db91efdb76
Don40 <87707102+Don40@users.noreply.github.com>
2025-03-14 17:21:30 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -16,7 +16,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"E:\python\python projects\Streamlit proj Dashboard")
+    os.chdir(r"E:\python\my-streamlit-app")
     df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
 
 col1, col2 = st.columns((2))
